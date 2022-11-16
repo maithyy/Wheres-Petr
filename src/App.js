503305e7a67f5ce8cc572844d0e47533ce8aeba6
Josh <jcpm2603@gmail.com>
2022-11-15 18:49:29 -0800

    Image displayer V1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import ClickCounter from './components/ClickCounter';
 import RandomizePetr from './components/RandomizePetr';
+import Upload from './components/DisplayImage';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Wheres Petr</h1>
-      <input type="file"  accept="image/*" name="image" id="file" /> <br /><br />
+      <input type="file"  accept="image/*" name="image" id="file"/> <br /><br />
       <ClickCounter/>
       <RandomizePetr/>
-
+      <Upload/>
     </div>
   );
 }
