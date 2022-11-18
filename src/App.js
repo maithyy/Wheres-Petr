7f6ccb0882aeaaf4f30001dc098aa8741cb3ead6
Maithy Le <maithyle265@yahoo.com>
2022-11-17 17:42:17 -0800

    Made App.js a bunch of routes and organized components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import RandomizePetr from "./components/RandomizePetr";
-import DisplayImage from "./components/DisplayImage";
 import ChangeImage from "./components/ChangeImage";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Wheres Petr</h1>
-      <RandomizePetr />
-      <DisplayImage />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<RandomizePetr />} />
+        <Route path="/picture" element={<ChangeImage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/IntroCards/IntroCards.jsx
@@ -0,0 +1,15 @@
+import "./IntroCards.css";
+
+function IntroCards(props) {
+  return (
+    <div className="CardContainer">
+      <div className="Card">
+        <img src={props.chosenImage} alt="Peter" />
+        <h2>{props.text}</h2>
+      </div>
+      <p>{props.caption}</p>
+    </div>
+  );
+}
+
+export default IntroCards;
